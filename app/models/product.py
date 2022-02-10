d27797b3b6d12aeced1fa7d34d6719c9e6523b11
Ilhan Mert <ilhanmertalan@gmail.com>
2022-02-10 22:38:55 +0300

    EDIT product name is not unique

--- app/models/product.py
+++ app/models/product.py
@@ -4,7 +4,7 @@ from app import db
 class Product(db.Model):
     __tablename__ = 'products'
     id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(64), index=True, unique=True)    
+    name = db.Column(db.String(64), index=True, unique=False)    
     price = db.Column(db.Float)
     description = db.Column(db.String(140))
     image = db.Column(db.String(140))
