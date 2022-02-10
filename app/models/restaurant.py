a5ae231c7adba417e1adbc2e4bc00fc68080a929
Ilhan Mert <ilhanmertalan@gmail.com>
2022-02-10 21:46:00 +0300

    ADD product-restaurant relationship

--- app/models/restaurant.py
+++ app/models/restaurant.py
@@ -6,6 +6,7 @@ class Restaurant(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(64), index=True, unique=True)
     userid = db.Column(db.Integer, db.ForeignKey('user.id'))
+    products = db.relationship('Product', backref='restaurants', lazy='dynamic')
 
     def __repr__(self):
         return '<Restaurant {}>'.format(self.name)
