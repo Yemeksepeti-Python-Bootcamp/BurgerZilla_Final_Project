f68966405a5be869e75f4c9d12edfe34329d5628
Ilhan Mert <ilhanmertalan@gmail.com>
2022-02-11 22:40:31 +0300

    ADD get user orders

--- app/api/user/controller.py
+++ app/api/user/controller.py
@@ -8,16 +8,37 @@ from .dto import UserDto
 
 api  = UserDto.api
 data_resp = UserDto.data_resp
+restaurant=UserDto.restaurant
+product=UserDto.product
+order=UserDto.order
 
 @api.route("/<string:username>")
 class UserGet(Resource):
-    @api.doc("get specified user data",responses={
+    @api.doc("get user details by username",responses={
         200:("Success",data_resp),
         404:"User Not Found",
     })
-    @jwt_required()
+    #@jwt_required()
     def get(self,username):
         """get user data"""
         return UserService.get_user_data(username)
     
 
+
+@api.route("/<int:user_id>")
+class UserOrders(Resource):
+    @api.doc("Get all orders of a user",responses={200:"Success",500:"Internal Server Error"})
+    #@jwt_required()
+    def get(self,user_id):
+        """
+        Get all orders of a user"""
+        return UserService.get_all_orders(user_id)
+
+@api.route("/orders/<int:order_id>")
+class UserOrder(Resource):
+    @api.doc("Get a specific order",responses={200:"Success",500:"Internal Server Error"})
+    #@jwt_required()
+    def get(self,order_id):
+        """
+        Get a specific order"""
+        return UserService.get_order_by_id(order_id)
--- app/api/user/service.py
+++ app/api/user/service.py
@@ -1,6 +1,12 @@
+from datetime import datetime
 from flask import current_app
-
-from app.utils import err_resp,internal_err_resp, message
+from app.models.schemas import RestaurantSchema
+from app.utils import err_resp,internal_err_resp,message
+from flask_jwt_extended import jwt_required, get_jwt_identity
+from app.models.restaurant import Restaurant
+from app.models.product import Product
+from app.models.order import Order
+from app import db
 from app.models.user import User
 
 class UserService:
@@ -22,4 +28,42 @@ class UserService:
             print("Error User:",e)
             current_app.logger.error(e)
             return internal_err_resp()
-            
+
+    @staticmethod
+    def get_all_orders(user_id):
+        """
+        Get all orders of a specific user
+        
+        """
+        if not(orders := Order.query.filter_by(userid=user_id)):
+            return err_resp("orders not found","orders_404",404)
+        from .utils import load_order_data
+        try:
+            orders_data = [load_order_data(order) for order in orders]
+            if len(orders_data)==0:
+                resp=message(True,"User has no orders yet")
+            else:
+                resp=message(True,"Orders loaded successfully")
+            resp["orders"]=orders_data
+            return resp,200
+        except Exception as e:
+            current_app.logger.error(e)
+            return internal_err_resp()
+    
+    @staticmethod
+    def get_order_by_id(order_id):
+        """
+        Get a specific order
+        
+        """
+        if not(order := Order.query.filter_by(id=order_id).first()):
+            return err_resp("order not found","order_404",404)
+        from .utils import load_order_data
+        try:
+            order_data = load_order_data(order)
+            resp=message(True,"Order loaded successfully")
+            resp["order"]=order_data
+            return resp,200
+        except Exception as e:
+            current_app.logger.error(e)
+            return internal_err_resp()
