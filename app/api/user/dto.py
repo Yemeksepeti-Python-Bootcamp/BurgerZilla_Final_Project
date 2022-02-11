5e221defff27bf78591157fd94d43fd86d5f4ec9
Ilhan Mert <ilhanmertalan@gmail.com>
2022-02-11 22:39:57 +0300

    ADD dto and utils for user endpoints

--- app/api/user/dto.py
+++ app/api/user/dto.py
@@ -16,3 +16,26 @@ class UserDto:
         "message":fields.String,
         "user":fields.Nested(user)
     })
+
+    restaurant = api.model("Restaurant",{
+        "id": fields.Integer(readOnly=True, description="The unique identifier of a restaurant"),
+        "name": fields.String(required=True, description="Restaurant name"),
+        "userid": fields.Integer(required=True, description="Restaurant owner id")
+    })
+
+    product = api.model("Product",{
+        
+        "name": fields.String(required=True, description="Product name"),
+        "price": fields.Float(required=True, description="Product price"),
+        "description": fields.String(required=True, description="Product description"),
+        "image": fields.String(required=True, description="Product image"),
+        "restaurantid": fields.Integer(required=True, description="Product restaurant id")
+    })
+
+    order = api.model("Order",{
+        "productid": fields.Integer(required=True, description="Product id"),
+        "restaurantid": fields.Integer(required=True, description="Restaurant id"),
+        "userid": fields.Integer(required=True, description="User id"),
+        "orderstatus": fields.String(required=True, description="Order status"),
+        "orderdate": fields.DateTime(required=True, description="Order date")
+    })
--- app/api/user/utils.py
+++ app/api/user/utils.py
@@ -5,4 +5,17 @@ def load_data(user_db_obj):
     user_schema = UserSchema()
     data = user_schema.dump(user_db_obj)
     return data
-    
+
+def load_product_data(product_db_obj):
+    #db objelerini schemaya uyarlama
+    from app.models.schemas import ProductSchema
+    product_schema = ProductSchema()
+    data = product_schema.dump(product_db_obj)
+    return data
+
+def load_order_data(order_db_obj):
+    #db objelerini schemaya uyarlama
+    from app.models.schemas import OrderSchema
+    order_schema = OrderSchema()
+    data = order_schema.dump(order_db_obj)
+    return data
