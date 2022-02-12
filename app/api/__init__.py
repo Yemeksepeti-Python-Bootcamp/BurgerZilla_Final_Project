1776731c076bcf9c56a1aced99a2d60e54249521
Ilhan Mert <ilhanmertalan@gmail.com>
2022-02-12 19:19:11 +0300

    ADD swagger authorization part

--- app/api/__init__.py
+++ app/api/__init__.py
@@ -5,8 +5,16 @@ from flask import Blueprint
 from .user.controller import api as user_ns
 from .restaurant.controller import api as restaurant_ns
 
+authorizations = {
+    'apikey': {
+        'type': 'apiKey',
+        'in': 'header',
+        'name': 'X-API-KEY'
+    }
+}
+
 api_bp = Blueprint("api", __name__)
-api = Api(api_bp, version="1.", title="API", description="API")
+api = Api(api_bp, version="1.", title="API", description="API",authorizations=authorizations)
 
 
 api.add_namespace(user_ns)
